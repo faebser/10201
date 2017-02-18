f53843a42852cb42e8c4a2fbee3eadc2fe319b71
Fabian Frei <fabian.frei@esf-frei.ch>
2017-02-18 21:08:28 +0100

    Lang settingswq

--- site/config/config.php
+++ site/config/config.php
@@ -28,3 +28,54 @@ make Kirby work. For more fine-grained configuration
 of the system, please check out http://getkirby.com/docs/advanced/options
 
 */
+
+// multilocale
+c::set('languages', array(
+  array(
+    'code'    => 'en',
+    'name'    => 'English',
+    'default' => true,
+    'locale'  => 'en_US',
+    'url'     => '/',
+  ),
+  array(
+    'code'    => 'de',
+    'name'    => 'Deutsch',
+    'locale'  => 'de_DE',
+    'url'     => '/de',
+  ),
+  array(
+    'code'    => 'fr',
+    'name'    => 'Francaise',
+    'locale'  => 'fr_FR',
+    'url'     => '/fr',
+  ),
+  array(
+    'code'    => 'it',
+    'name'    => 'Italiano',
+    'locale'  => 'it_IT',
+    'url'     => '/it',
+  ),
+  array(
+    'code'    => 'de',
+    'name'    => 'Deutsch',
+    'locale'  => 'de_CH',
+    'url'     => '/de',
+  ),
+  array(
+    'code'    => 'fr',
+    'name'    => 'Francaise',
+    'locale'  => 'fr_CH',
+    'url'     => '/fr',
+  ),
+  array(
+    'code'    => 'it',
+    'name'    => 'Italiano',
+    'locale'  => 'it_CH',
+    'url'     => '/it',
+  )
+));
+// lang auto detect
+c::set('language.detect', true);
+// changed date handler for langs
+c::set('date.handler', 'strftime'); 
