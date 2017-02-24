7e30a7557467ce6f7b7468bfd360eba8d4d18ea2
Fabian Frei <fabian.frei@esf-frei.ch>
2017-02-24 18:37:04 +0100

    Added snippet for organs

--- site/templates/default.php
+++ site/templates/default.php
@@ -3,7 +3,7 @@
   <main class="main" role="main">
    <?php
       foreach($pages->visible() as $section) {
-        echo $section->intendedTemplate() . " "; 
+        echo $section->intendedTemplate() . " ";
         snippet($section->intendedTemplate(), array('data' => $section));
       }
     ?>
